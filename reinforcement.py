12f5855605779da8b6c3f90b69aa3780bdf2a99f
Vincent Couverchel <vincent.couverchel@epitech.eu>
2018-01-03 00:03:49 +0100

    change board shape and save format

--- reinforcement.py
+++ reinforcement.py
@@ -8,18 +8,16 @@ save labels (state, policy, winning) to file label_{num_game}.label
 import numpy as np
 from mc_tree_search import turn, expand
 from node import Node
+import os
 
 def save_tmp_label(turns, board, p, player):
     """
     append board, policy vector and current player to tmp file
-    format => np.array => ((19, 19, 3), 19 * 19, 1) => 19, 19, 5
+    format => np.array => ((19, 19, 3), 19 * 19, 1)
     """
-    p.shape = (19,19)
-    save = np.insert(board, 3, p, axis=2)
-    player_layer = np.zeros((19, 19))
-    player_layer[0, 0] = player
-    save = np.insert(save, 4, p, axis=2)
-    turns.append(save)
+    turns[0].append(board)
+    turns[1].append(p)
+    turns[2].append(player)
 
 def save_final_label(turns, filename, winner):
     """
@@ -27,20 +25,24 @@ def save_final_label(turns, filename, winner):
     format => np.array => ((19, 19, 3), 19 * 19, 1) => 19, 19, 5
     """
     # turns.apply(lambda turn: turn[0, 0, 4] = 1 if turn[0, 0, 4] == winner else -1)
-    for i in range(len(turns)):
-        turns[i][0, 0, 4] = 1 if turns[i][0, 0, 4] == winner else -1
-    turns = np.array(turns)
+    for i in range(len(turns[2])):
+        turns[i] = 1 if turns[i] == winner else -1
+    turns[0] = np.array(turns[0])
+    turns[1] = np.array(turns[1])
+    turns[2] = np.array(turns[2])
     if os.path.exists(filename):
-        old_turns = np.load(filename)
-        turns = np.append(old_turns, turns, axis=0)
-    np.save(filename, turns)
+        old_array = np.load(filename)
+        turns[0] = np.append(old_array['boards'], turns[0], axis=0)
+        turns[1] = np.append(old_array['p'], turns[1], axis=0)
+        turns[2] = np.append(old_array['z'], turns[2], axis=0)
+    np.savez(filename, board=turns[0], p=turns[1], z=turns[2])
 
 
 def print_board(board):
     """
     debug, print current map to term with colours
     """
-    board = (board[:,:,:,0] + board[:,:,:,1] * 2)[0]
+    board = (board[:,:,0] + board[:,:,1] * 2)
     print ("board:")
     for line in board:
         l = ""
@@ -58,7 +60,7 @@ def init_game(network_1, network_2):
     init game board, first node, next player turn
     """
     # init board
-    board = np.zeros((1, 19, 19, 3), np.int8)
+    board = np.zeros((19, 19, 3), np.int8)
     # init player 1 tree
     node_p_1 = Node(0)
     expand(node_p_1, board, 0, network_1)
@@ -73,7 +75,7 @@ def update_turn(board, player, node, network, pos):
     return child node according to opponent move
     """
     # get child from number of empty moves before opponent move
-    complete_board = (board[:,:,:,0] + board[:,:,:,1]).flatten()
+    complete_board = (board[:,:,0] + board[:,:,1]).flatten()
     child = node.get_child(np.sum((complete_board[:(pos[1] * 19 + pos[0])] == 0)))
     # if unexplored yet
     if child.leaf():
@@ -84,7 +86,7 @@ def turn_sequence(board, player, node_current, net_current, node_opponent, net_o
     """
     perform a complete turn
     take state info, player objects, and save boolean
-    return game status (O, 1), updated cuurent and opponent nodes
+    return game status (O, 1), updated current and opponent nodes
     """
     # run mcts simulation: return pos move, new board, policy p, new node, game status
     pos, board, p, node_current, status = turn(board, player, node_current, net_current)
@@ -104,15 +106,15 @@ def game(network_1, network_2, filename):
     """
     board, node_p_1, node_p_2 = init_game(network_1, network_2)
     
-    turns = []
+    turns = ([], [], [])
     while (True):
 
-        status, node_p_1, node_p_2 = turn_sequence(board, 0, node_p_1, network_1, node_p_2, network_2, None)
+        status, node_p_1, node_p_2 = turn_sequence(board, 0, node_p_1, network_1, node_p_2, network_2, turns)
         if status:
             save_final_label(turns, filename, 0)
             return
 
-        status, node_p_2, node_p_1 = turn_sequence(board, 1, node_p_2, network_2, node_p_1, network_1, None)
+        status, node_p_2, node_p_1 = turn_sequence(board, 1, node_p_2, network_2, node_p_1, network_1, turns)
         if status:
             save_final_label(turns, filename, 1)
             return
