b7390805dc07b8b1321467f0f6ab1e1d17b3eb34
moahmed-laid <mohamed-laid.hedia@epitech.eu>
2018-01-09 17:20:45 +0100

    fix bugs with multiple graph

--- src/network.py
+++ src/network.py
@@ -15,18 +15,22 @@ class Network(object):
         self.version = version
 
         if os.path.isfile(path_to_restore_model):
+            print("restore")
             saver = tf.train.Saver()
             saver.restore(self._sess, path_to_restore_model)
         else:
-            self._state = tf.placeholder(tf.float32, shape=[None, 19, 19, 3])
-            self._p_head, self._v_head = network(self._state)
-            self._loss, self._train_p_mcts, self._train_winner = loss_function(self._state,
-                                                                               self._p_head,
-                                                                               self._v_head)
-            self._glob = tf.global_variables_initializer()
-            self._sess = tf.Session()
-
-        self._sess.run(self._glob)
+            self._graph = tf.Graph()
+            with self._graph.as_default():
+                self._state = tf.placeholder(tf.float32, shape=[None, 19, 19, 3])
+                self._p_head, self._v_head = network(self._state)
+                self._loss, self._train_p_mcts, self._train_winner = loss_function(self._state,
+                                                                                   self._p_head,
+                                                                                   self._v_head)
+                self._glob = tf.global_variables_initializer()
+                self._sess = tf.Session(graph=self._graph)
+                self._sess.run(self._glob)
+                writer = tf.summary.FileWriter('./logs')
+                writer.add_graph(self._graph)
 
     def infer(self, board):
         """
@@ -36,9 +40,10 @@ class Network(object):
                               feed_dict={self._state: board[None, :]})
 
     def save_session(self):
-        saver = tf.train.Saver()
-        saver_path = saver.save(self._sess, "../models/model-" + str(self.version) + ".ckpt")
-        print("model saved in %s" %saver_path)
+        with self._graph.as_default():
+            saver = tf.train.Saver()
+            saver_path = saver.save(self._sess, "../models/model-" + str(self.version) + ".ckpt")
+            print("model saved in %s" %saver_path)
         return
 
     def train(self, board, p, z):
@@ -96,73 +101,79 @@ def conv_layer(input):
     batch normalization and rectifier non linearity (reLU)
     """
 
-    conv = convolution(input=input, filters=64, ksize=3)
-    bn = tf.layers.batch_normalization(conv)
-    relu = tf.nn.relu(bn)
-    return relu
+    with tf.variable_scope('conv'):
+        conv = convolution(input=input, filters=64, ksize=3)
+        bn = tf.layers.batch_normalization(conv)
+        relu = tf.nn.relu(bn)
+        return relu
 
-def res_layer(input):
+def res_layer(input, id):
     """
     Implementation of a residual layer with 64 filter (3x3)
     """
-
-    conv = convolution(input=input, filters=64, ksize=3)
-    bn = tf.layers.batch_normalization(conv)
-    relu = tf.nn.relu(bn)
-    conv = convolution(input=relu, filters=64, ksize=3)
-    bn = tf.layers.batch_normalization(conv)
-    skip = tf.add(bn, input)
-    relu = tf.nn.relu(skip)
-    return relu
+    with tf.variable_scope('res_' + str(id) ) as scope:
+        conv = convolution(input=input, filters=64, ksize=3)
+        bn = tf.layers.batch_normalization(conv)
+        relu = tf.nn.relu(bn)
+        conv = convolution(input=relu, filters=64, ksize=3)
+        bn = tf.layers.batch_normalization(conv)
+        skip = tf.add(bn, input)
+        relu = tf.nn.relu(skip)
+        return relu
 
 def value_head(input):
     """
     The value head
     """
-    conv = convolution(input=input, filters=1, ksize=1)
-    bn = tf.layers.batch_normalization(conv)
-    relu = tf.nn.relu(bn)
-    flatten = tf.reshape(relu, [-1, 19 * 19 * 1])
-    fc = fully_connected(flatten, units=256)
-    relu = tf.nn.relu(fc)
-    fc = fully_connected(relu, units=1)
-    tanh = relu = tf.nn.tanh(fc)
-    output = tanh[:, 0]
-    return output
+
+    with tf.variable_scope('value_head'):
+        conv = convolution(input=input, filters=1, ksize=1)
+        bn = tf.layers.batch_normalization(conv)
+        relu = tf.nn.relu(bn)
+        flatten = tf.reshape(relu, [-1, 19 * 19 * 1])
+        fc = fully_connected(flatten, units=256)
+        relu = tf.nn.relu(fc)
+        fc = fully_connected(relu, units=1)
+        tanh = relu = tf.nn.tanh(fc)
+        output = tanh[:, 0]
+        return output
 
 def policy_head(input):
     """
     The policy head
     """
-    conv = convolution(input=input, filters=2, ksize=1)
-    bn = tf.layers.batch_normalization(conv)
-    relu = tf.nn.relu(bn)
-    flatten = tf.reshape(relu, [-1, 19 * 19 * 2])
-    fc = fully_connected(flatten, units=19 * 19)
-    tanh = tf.nn.tanh(fc)
-    return tanh
+    with tf.variable_scope('policy_head'):
+        conv = convolution(input=input, filters=2, ksize=1)
+        bn = tf.layers.batch_normalization(conv)
+        relu = tf.nn.relu(bn)
+        flatten = tf.reshape(relu, [-1, 19 * 19 * 2])
+        fc = fully_connected(flatten, units=19 * 19)
+        tanh = tf.nn.tanh(fc)
+        return tanh
 
 def network(input):
+    print('network')
     layer = conv_layer(input)
-    for _ in range(16):
-        layer = res_layer(layer)
+    for i in range(16):
+        layer = res_layer(layer, i)
     policy = policy_head(layer)
     value = value_head(layer)
     return policy, value
 
 def loss_function(state, p_head, v_head):
 
-    c = 0.01
-    p = tf.placeholder(tf.float32, [None, 19 * 19])
-    z = tf.placeholder(tf.float32, [None])
+    with tf.variable_scope('loss'):
+        c = 0.01
+        p = tf.placeholder(tf.float32, [None, 19 * 19])
+        z = tf.placeholder(tf.float32, [None])
 
-    mean_square = tf.reduce_mean(tf.squared_difference(v_head, z))
-    cross_entropy = tf.reduce_mean(-tf.reduce_sum(p * tf.log(p_head), reduction_indices=[1]))
+        mean_square = tf.reduce_mean(tf.squared_difference(v_head, z))
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(p * tf.log(p_head), reduction_indices=[1]))
 
-    regularizer = tf.nn.l2_loss(state)
-    loss = mean_square + cross_entropy + c * regularizer
+        regularizer = tf.nn.l2_loss(state)
+        loss = mean_square + cross_entropy + c * regularizer
 
-    return loss, p, z
+        return loss, p, z
 
 """
 if __name__ == '__main__':
